a483403bf2454ba3b0ab8e3d7d7b5a05d3d148c1
tlacolley <thomaslacolley@gmail.com>
2018-11-07 13:56:39 +0100

    Creation and style footer

--- footer.php
+++ footer.php
@@ -1,5 +1,29 @@
 
 
 <?php wp_footer(); ?>
+<footer class="mainFooter">
+    <section class="helpFooter">
+        <h3>Help</h3>
+        <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Return Policy</a></li>
+            <li><a href="#">Contact</a></li>
+
+        </ul>
+    </section>
+    <section class="aboutFooter">
+        <h3>About</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    </section>
+    <section class="newsletterFooter">
+        <h3>Newsletter</h3>
+        <form class="formNewsletter" action="." method="post">
+            <input type="text" name="inputMailNewsletter" placeholder="Your email address" value="">
+            <button type="submit" name="button">Sign up</button>
+        </form>
+    </section>
+</footer>
 </body>
 </html>
--- header.php
+++ header.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>CodiShop</title>
         <?php wp_head() ?>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     </head>
     <body>
 
@@ -13,10 +14,10 @@
     <nav class="mainNav">
         <a href="<?php echo home_url("") ?>"><h1>CodiShop</h1></a>
         <?php wp_nav_menu() ?>
-        <ul>
-            <li><a href="#">Icon</a></li>
-            <li><a href="#">Icon</a></li>
-            <li><a href="#">Icon</a></li>
+        <ul class="iconList">
+            <li><a href="#"><i class="fas fa-user-astronaut"></i></a></li>
+            <li><a href="#"><i class="fas fa-shopping-bag"></i></a></li>
+            <li><a href="#"><i class="fas fa-search"></i></a></li>
 
         </ul>
     </nav>
